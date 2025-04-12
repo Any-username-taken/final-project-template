cefbc3f3188fea141253dd6e17b706e922c6169c
Any-username-taken <deetrik.morales09@gmail.com>
2025-04-12 04:19:40 +0000

    Started remaking from ground up cause the tutorial was getting on my nerves. I think I have the hang of classes in javascript better though. We can only hope it works.

--- script.js
+++ script.js
@@ -4,10 +4,6 @@
 let player;
 let b_ground;
 let stars1;
-let stars2;
-let stars3;
-let stars4;
-let stars5;
 
 // 6.3 === full rotation new Component(..., angle)
 // anim var
@@ -17,6 +13,10 @@ let imagesScale = 0.2;
 function startGame() {
     GameArea.start();
     player = new Component(313*imagesScale, 207*imagesScale, playerSprite,  640-(313*imagesScale)/2, 360-(202*imagesScale)/2, "player", 6.3)
+
+    //Get Input
+    window.addEventListener("keydown", handleMovementPress);
+    window.addEventListener("keyup", handleMovementRelease);
 }
 
 //Canvas creation
@@ -79,10 +79,162 @@ function Component (width, height, source, x, y, type, angle) {
     }
 }
 
+
+class Sprite{
+    constructor(imgPar, pos, type, angle, health) {
+        this.imgPar = imgPar //Needs {width: none, height: none, source: none}
+
+        this.image = new Image();
+        this.image.src = imgPar.source; //Remember to make imgPar an obj in all instances
+        
+        this.pos = pos;
+        
+        this.velX = 0;
+        this.velY = 0;
+
+        this.type = type
+
+        this.angle = angle
+
+        this.health = health
+
+        //Tracking keypresses avoids headache
+        this.keypress = {
+            w: false,
+            a: false,
+            s: false,
+            d: false
+        }
+    }
+
+    update() {
+        //New update function sure to bring joy to the everybody.
+
+        let ctx = GameArea.context;
+
+        ctx.save();
+
+        //rotate player img
+        ctx.translate(this.pos.x + this.imgPar.width/2, this.pos.y + this.imgPar.height/2);
+        ctx.rotate(this.angle);
+
+        //Images anim
+        
+        ctx.drawImage(this.image, -this.imgPar.width/2, -this.imgPar.height/2, this.imgPar.width, this.imgPar.height);
+
+        ctx.restore();
+    }
+}
+
+
+class Player extends Sprite{
+    constructor(imgPar, pos, type, angle, health, firerate) {
+        super(imgPar, pos, type, angle, health)
+
+        this.firerate = firerate
+    }
+
+    refresh() {
+        this.get_input()
+        this.get_translate()
+        super.update()
+    }
+    
+    get_translate() {
+        this.pos.y += this.velX
+        this.pos.x += this.velY
+        
+        
+
+        //Screen wrap top-bottom
+
+        if (this.pos.y > 576) {
+            this.pos.y = -100
+        }
+
+        if (this.pos.y < -100) {
+            this.pos.y = 576
+        }
+
+        //Screen wrap left-right
+        
+        if (this.pos.x > 1024) {
+            this.pos.x = -100
+        }
+
+        if (this.pos.x < -100) {
+            this.position.x = 1024
+        }
+
+        //Lower velocity if high
+
+        if (this.velX > 0) {
+            this.velX -= 0.125
+        }
+
+        if (this.velX < 0) {
+            this.velX += 0.125
+        }
+
+        // for the y velocity now
+
+        if (this.velY > 0) {
+            this.velY -= 0.125
+        }
+
+        if (this.velY < 0) {
+            this.velY += 0.125
+        }
+    }
+
+    get_keydowns() {
+
+    }
+
+    get_input() {
+        document.addEventListener("keydown", (event) => {
+            if (event.key === "w") {
+                this.keypress.w = true;
+            }
+
+            if (event.key === "a") {
+                this.keypress.a = true;
+            }
+
+            if (event.key === "s") {
+                this.keypress.s = true;
+            }
+
+            if (event.key === "d") {
+                this.keypress.d = true;
+            }
+        })
+
+        document.addEventListener("keyup", (event) => {
+            if (event.key === "w") {
+                this.keypress.w = false;
+            }
+
+            if (event.key === "a") {
+                this.keypress.a = false;
+            }
+
+            if (event.key === "s") {
+                this.keypress.s = false;
+            }
+
+            if (event.key === "d") {
+                this.keypress.d = false;
+            }
+        })
+    }
+}
+
 function updateGameArea() {
     GameArea.clear();
 
     let ctx = GameArea.context;
 
-    player.update()
+    enemy.update()
+
 }
